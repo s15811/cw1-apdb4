a1455d6d3977972533d3dc3e080d6aeb8ebcf80b
s15811 <s15811@pjwstk.edu.pl>
2020-03-01 16:14:32 +0100

    Dodanie projektu

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cwiczenia1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        { 
+            var httpClient = new HttpClient();
+            var respose = await httpClient.GetAsync("https://pja.edu.pl");
+            //var respose = await httpClient.GetAsync(args[0]);
+            //JS Promise async/await
+            //Java Future
+            //C# Task async/await
+
+            if (respose.IsSuccessStatusCode)
+            {
+                string html = await respose.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z0-9]+\\.[a-z0-9]", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(html);
+
+                foreach(var m in matches){
+                    Console.WriteLine(m);
+                }
+                
+            }
+
+
+        }
+    }
+}
