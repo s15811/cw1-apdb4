98240a21208ce48434d387f5c6ce947400282369
DonArturooo <40776354+DonArturooo@users.noreply.github.com>
2020-03-01 23:52:13 +0100

    odatkowe-wymagania

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections;
 
 namespace Cwiczenia1
 {
@@ -10,7 +11,17 @@ namespace Cwiczenia1
         public static async Task Main(string[] args)
         { 
             var httpClient = new HttpClient();
-            //var respose = await httpClient.GetAsync("https://pja.edu.pl");
+
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!CheckURLValid(args[0]))
+            {
+                throw new ArgumentException();
+            }
+
             var respose = await httpClient.GetAsync(args[0]);
 
             //JS Promise async/await
@@ -20,17 +31,50 @@ namespace Cwiczenia1
             if (respose.IsSuccessStatusCode)
             {
                 string html = await respose.Content.ReadAsStringAsync();
-                var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z0-9]+\\.[a-z0-9]", RegexOptions.IgnoreCase);
-
+                var regex = new Regex("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,63}", RegexOptions.IgnoreCase);
                 var matches = regex.Matches(html);
-
-                foreach(var m in matches){
-                    Console.WriteLine(m);
-                }
+                Hashtable hash = new Hashtable();
                 
+
+                if (matches.Count == 0)
+                {
+                    Console.WriteLine("Nie znaleziono adresów email");
+                }
+                else
+                {
+                    foreach (Match m in matches)
+                    {
+                        string foundMatch = m.ToString();
+                        if (hash.Contains(foundMatch) == false)
+                        {
+                            hash.Add(foundMatch, string.Empty);
+                        }
+                    }
+
+                    foreach (DictionaryEntry element in hash)
+                    {
+                        Console.WriteLine(element.Key);
+                    }
+                }
+                httpClient.Dispose();
+            }
+            else
+            {
+                Console.WriteLine("Błąd w czasie pobrania strony");
+                httpClient.Dispose();
             }
 
 
         }
+
+        public static bool CheckURLValid(string url)
+        {
+            if (url == null)
+            {
+                return false;
+            }
+            return Regex.IsMatch(url, "(http|https)://(([www\\.])?|([\\da-z-\\.]+))\\.([a-z\\.]{2,3})");
+        }
+
     }
 }
